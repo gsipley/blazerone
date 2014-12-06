f488e185ebeeed8a90a189b42ce19e7164672683
Mercer Hall <mercerhall@hotmail.com>
2014-12-05 23:14:47 -0500

    fixed diagnostics new

--- blazerone/config/routes.rb
+++ blazerone/config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  get 'pages/about'
 
+  root 'pages#welcome'
   get 'pages/welcome'
+  get 'pages/about'
+  get 'diagnostics/new'
 
-  resources: diagnostics
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
