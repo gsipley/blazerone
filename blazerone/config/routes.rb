5badcb208be3be08d9a17da5bef08ff8d527b527
Gina Sipley <gsipley@gmail.com>
2014-12-06 17:22:09 -0500

    html

--- blazerone/config/routes.rb
+++ blazerone/config/routes.rb
@@ -1,13 +1,21 @@
-Rails.application.routes.draw do
-  get 'pages/about'
+# resources: pages
 
+Rails.application.routes.draw do
+ 
   get 'pages/welcome'
+
+  get 'pages/about' 
+
+ 
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  # You can have the root of your site routed with "root"  'welcome#index' 
+  # root 'pages#welcome'
+  
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
