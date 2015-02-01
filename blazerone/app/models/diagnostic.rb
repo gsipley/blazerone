12b6921242c8593c062fee7ac8f1a2f7fc7ba90e
Gina Sipley <gsipley@gmail.com>
2015-01-31 19:13:29 -0500

    added attributes to Diagnostic model

--- blazerone/app/models/diagnostic.rb
+++ blazerone/app/models/diagnostic.rb
@@ -2,5 +2,7 @@ class Diagnostic < ActiveRecord::Base
 
 	# validates :user_id, presence: true
 	belongs_to :user
-
+def diagnostic_score
+		answer1.to_i + answer2.to_i + answer3.to_i + answer4.to_i
+	end
 end
